c56b51e81d1b4a0e1a0cc314016a5e3dde31e7bc
Mahaut <mahautdeguilhem@gmail.com>
2019-11-06 00:26:00 +0100

    allow push

--- config/application.rb
+++ config/application.rb
@@ -8,13 +8,15 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-  #pour permettre le push sur heroku
-config.assets.initialize_on_precompile = false
 
 module Eventbrite
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+
+  #pour permettre le push sur heroku
+config.assets.initialize_on_precompile = false
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
