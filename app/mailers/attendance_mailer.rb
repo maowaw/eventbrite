defe984c9977006e691df723e922fe331e4a4d2e
Mahaut <mahautdeguilhem@gmail.com>
2019-11-06 01:33:56 +0100

    allow create event on heroku

--- app/mailers/attendance_mailer.rb
+++ app/mailers/attendance_mailer.rb
@@ -1,16 +1,16 @@
 class AttendanceMailer < ApplicationMailer
 
-default from: 'no-reply@monsite.fr'
+# default from: 'no-reply@monsite.fr'
  
-  def participation_email(attendance)
-    #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
-    @attendance = attendance 
+#   def participation_email(attendance)
+#     #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
+#     @attendance = attendance 
 
-    #on définit une variable @url qu'on utilisera dans la view d’e-mail
-    @url  = 'http://monsite.fr/login' 
+#     #on définit une variable @url qu'on utilisera dans la view d’e-mail
+#     @url  = 'http://monsite.fr/login' 
 
-    # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
-    mail(to: @attendance.user.email, subject: 'Prêt à faire la fête ?') 
-  end
+#     # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
+#     mail(to: @attendance.user.email, subject: 'Prêt à faire la fête ?') 
+#   end
 
 end
